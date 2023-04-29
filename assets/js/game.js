09dea4129bf475758d220197b96baccc33b9bc7a
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-04-29 23:34:56 +0200

    last and high score with localstorage

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,10 @@ class Game {
     constructor() {
         this.fps = 30;
         this.score = 0;
+        const storedLastScore = JSON.parse(localStorage.getItem("last-score"));
+        const storedHighScore = JSON.parse(localStorage.getItem("high-score"));
+        this.lastScore = storedLastScore ? storedLastScore : 0;
+        this.highScore = storedHighScore ? storedHighScore : 0;
         this.board = new Board();
         this.currentPiece = new Piece(this);
         this.addEventListeners();
@@ -14,10 +18,20 @@ class Game {
     }
 
     restart() {
+        this.lastScore = this.score;
+        if (this.score > this.highScore) this.highScore = this.score;
         this.score = 0;
+        this.storeScores();
         this.board.emptyBoard();
         this.currentPiece.nextType = Math.floor(Math.random() * Piece.pieceCount);
         this.currentPiece.reset();
+    }
+
+    storeScores() {
+        if (localStorage) {
+            localStorage.setItem("last-score", JSON.stringify(this.lastScore));
+            localStorage.setItem("high-score", JSON.stringify(this.highScore));
+        }
     }
 
     draw() {
@@ -30,6 +44,8 @@ class Game {
 
     updateScore() {
         document.querySelector('p#score').innerText = `score: ${this.score}`;
+        document.querySelector('p#last-score').innerText = `last score: ${this.lastScore}`;
+        document.querySelector('p#high-score').innerText = `high score: ${this.highScore}`;
     }
 
     handleKeyPress(key) {
